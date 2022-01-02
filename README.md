d6d69e654f10e53fd286607c8a266c5013141aa1
倚梦清风 <78645176+ymqf@users.noreply.github.com>
2022-01-02 14:17:44 +0800

    Add files via upload
    搬运个人介绍

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# me
+## 个人简历
+非作者
+原作者：https://gitee.com/n0ts
+[个人简历 - 田琪扬 (n0ts.cn)](https://me.n0ts.cn/)
+
+
---
+++ js/data.js
@@ -0,0 +1,306 @@
+export default {
+    index: {
+        title: "你好，我是",
+        me: [
+            "龙金升",
+            "高中生",
+            "计算机狂热粉"
+        ],
+        bg: "./images/bg.jpg",
+        subTitle: "即使前方的路看似绝境，也要有硬生生给自己开出一条路的勇气",
+        contact: [{
+                name: "Blog",
+                icon: "fa-wordpress",
+                link: "http://blog.52yimen.cn/"
+            },
+            {
+                name: "Email",
+                icon: "fa-envelope",
+                link: "574686795@qq.com"
+            },
+            {
+                name: "Gitee",
+                icon: "fa-github",
+                link: "http://gitee.com/"
+            },
+            {
+                name: "QQ",
+                icon: "fa-qq",
+                link: "http://wpa.qq.com/msgrd?v=3&uin=574686795&site=qq&menu=yes"
+            }
+        ],
+        loadMore: {
+            text: "了解更多",
+            class: "lovexhj2"
+        }
+    },
+ about: {
+     title: "关于我",
+     laozi: {
+         img: "./images/55.png",
+         content: "<span></span>我叫<b>龙金升</b>，今年16岁，是一名普通，<b>高中生</b>；<br /><span></span><b>计算机</b>，是我的兴趣<喜欢花时间鼓捣一些乱七八糟的东西,目前还没有系统化的学习编程语言一直在拖延当中.....<span></span><br>每一天都很无聊,不喜不悲大概就是我最好的心情咯。</br> 正在努力考上好大学,期待能认识更多有趣的人. <br>别看旁边的图片~"
+     },
+     ability: [{
+             title: "动漫",
+             icon: "fa-html5",
+             subTitle: "jojo ,人或者就是为了麻衣学姐！！！",
+             color: "#ff4757",
+             bfb: "80%"
+         },
+         {
+             icon: "fa-code",
+             title: "游戏",
+             subTitle: "游戏什么的，我不知道该干什么.",
+             color: "#ffa502",
+             bfb: "20%"
+         },
+         {
+             icon: "fa-linux",
+             title: "平凡",
+             subTitle: "没有人想在年少时成为一个普通人",
+             color: "#1e90ff",
+             bfb: "90%"
+         },
+         {
+             icon: "fa-photo",
+             title: "幼稚",
+             subTitle: "嗯  貌似没错",
+             color: "#2ed573",
+             bfb: "80%"
+         }
+     ],
+     loadMore: {
+         text: "继续浏览",
+         class: "lovexhj3"
+     }
+ },
+    project: {
+        title: "我的项目",
+        list: [{
+                name: "峰峰二中表白墙",
+                text: "表白墙！",
+                nb: [
+                    "什么不会"
+                ],
+                url: "http://wall.52yimen.cn/",
+                img: "./images/tucao1.png",
+                content: `<h2>项目类型</h2>
+                <p>练手项目</p>
+                <h2>开发周期</h2>
+                <p>2021年2月2日 – 2021年3月12日，5人/38天</p>
+                <h2>开发工具</h2>
+                <p>什么不会</p>
+                <h2>项目背景</h2>
+				<p>快乐减压，健康生活，是本网站的主旨。表白墙是以帮助单身狗脱单，轻松休闲。在表白墙可以查看他人发布的，也可以与其他的人分享自己亲身经历或听说到的悲剧、搞笑、有意思的、不顺心的等各类情形的生活中的事。</p>
+				<h2>介绍</h2>
+				<p>最开始玩的东西.</p>
+				<p>但是由于各种原因宣传不起来,所以知道的人数很少咯~</p>
+				<p>~</p>
+                
+               <h2>项目预览</h2>
+                <ul>
+                    <li><img src="./images/tucao1.png"></li>
+                    <li><img src="./images/tucao2.png"></li>
+                    <li><img src="./images/tucao3.png"></li>
+                    <li><img src="./images/tucao4.png"></li>
+                </ul>
+                <h2>项目职责</h2>
+                <p>1、代码搬运工p>
+                `
+            },
+            {
+                name: "个人博客",
+                text: "记录美好生活！",
+                nb: [
+                    "什么也不会"
+                ],
+                url: "http://www.52yimen.cn/",
+                img: "./images/tucao3.png",
+                content: `<h2>项目类型</h2>
+                <p>练手项目</p>
+                <h2>开发周期</h2>
+                <p>很长时间</p>
+                <h2>开发工具</h2>
+                <p>博客框架Typecho</p>
+                <h2>项目背景</h2>
+                <p>记录下青春,</p>
+                <h2>模块介绍</h2>
+				<li><img src="./images/5456.png"></li>
+               <p>发布文章</p>
+			   <p>主题</p>
+                <h2>项目预览</h2>
+                <ul>
+                    
+                    <li><img src="./images/tucao3.png"></li>
+                    <li><img src="./images/tucao4.png"></li>
+                </ul>
+                <h2>项目职责</h2>
+                <p>1、代码搬运；</p>
+                <p>2、1；</p>
+                <p>3、1；</p>`,
+            },
+            {
+                name: "峰峰二中表白墙小程序",
+                text: "微信搜索:峰峰二中表白墙",
+                nb: [
+                    "Winform", "SQLServer", "C#"
+                ],
+                url: "http://www.52yimen.cn/",
+                img: "./images/bg2.jpg",
+                content: `<h2>项目类型</h2>
+                <p>练手项目</p>
+                <h2>开发周期</h2>
+                <p>微信搜索:峰峰二中表白墙!,</p>
+                <h2>开发工具</h2>
+                <p>Visual Studio、SQLServer、Git</p>
+                <h2>项目背景</h2>
+                <p>微信搜索:峰峰二中表白墙</p>
+                <h2>模块介绍</h2>
+                <p>1、	微信搜索:峰峰二中表白墙</p>
+             
+                <h2>项目技术</h2>
+                <p>1、	微信搜索:峰峰二中表白墙；</p>
+                <h2>项目预览</h2>
+                <ul>
+                    <li><img src="./images/xcl1.png"></li>
+                    <li><img src="./images/xcl2.png"></li>
+                    <li><img src="./images/xcl3.png"></li>
+                </ul>
+                <h2>项目职责</h2>
+                
+                <p>4、空!；</p>`,
+            },
+            {
+                name: "峰峰二中影视网",
+                text: "免费看全网vip电影",
+                nb: [
+                    "Gitee Issues", "Vue", "Vue", "Linux", "SEO", "CDN"
+                ],
+                url: "http://www.52yimen.cn/",
+                img: "./images/heng1.png",
+                content: `<h2>项目类型</h2>
+                <p>练手项目</p>
+                <h2>开发周期</h2>
+                <p>空!</p>
+                <h2>开发工具</h2>
+                <p>空!</p>
+                <h2>项目背景</h2>
+                <p>免费看全网vip电影</p>
+                <h2>模块介绍</h2>
+                <p>1、	添加我 我发你；</p>
+                <h2>项目技术</h2>
+                <p>1、	待添加；</p>
+              
+                <h2>项目预览</h2>
+                <ul>
+                    <li><img src="./images/heng1.png"></li>
+                    
+                </ul>`,
+            },
+            {
+                name: "待添加",
+                text: "空",
+                nb: [
+                    "Vue", "Vue", "Linux", "SEO", "CDN"
+                ],
+                url: "http://www.52yimen.cn/",
+                img: "./images/peng.png",
+                content: `<h2>项目类型</h2>
+                <p>练手项目</p>
+                <h2>开发周期</h2>
+                <p>很长时间~~</p>
+                <h2>开发工具</h2>
+                <p>套源码</p>
+                <h2>项目背景</h2>
+                <p>想写点东西记录</p>
+                <h2>模块介绍</h2>
+                
+                <p>文章发布,主题,.....</p>`,
+            },
+            {
+                name: "待添加",
+                text: "待添加",
+                nb: [
+                    "待添加", "Linux", "SEO", "CDN"
+                ],
+                url: "http://www.52yimen.cn/",
+                img: "./images/geek1.png",
+                content: `<h2>项目类型</h2>
+                <p>待添加</p>
+                <h2>开发周期</h2>
+                <p>空</p>
+                <h2>开发工具</h2>
+                <p>空</p>
+                <h2>项目背景</h2>
+                <p>空</p>
+                <h2>模块介绍</h2>
+                <p>1、	待添加；</p>
+                
+                <h2>项目技术</h2>
+                <p>1、	待添加；</p>
+                
+                <h2>项目预览</h2>
+                <ul>
+                    <li><img src="./images/geek1.png"></li>
+                    <li><img src="./images/geek2.png"></li>
+                    <li><img src="./images/geek3.png"></li>
+                    <li><img src="./images/geek4.png"></li>
+                </ul>`,
+            },
+           
+            
+          
+            {
+                name: "待添加",
+                text: "待添加",
+                nb: [
+                    "待添加L", "CSS", "Js", "PHP", "Wordpress"
+                ],
+                url: "http://www.52yimen.cn/",
+                img: "./images/no1.png",
+                content: `<h2>项目类型</h2>
+                <p>1</p>
+                <h2>空</h2>
+                <p>空</p>
+                <h2>开发工具</h2>
+                <p>空</p>
+                <h2>项目背景</h2>
+                <p>空</p>
+                <h2>项目技术</h2>
+                <p>1、 空；</p>
+                
+                <h2>项目预览</h2>
+                <ul>
+                    <li><img src="./images/no1.png"></li>
+                    <li><img src="./images/no2.png"></li>
+                    <li><img src="./images/no3.png"></li>
+                </ul>`,
+            }
+        ],
+        listLoadMore: "查看",
+        loadMore: {
+            text: "继续浏览",
+            class: "lovexhj4"
+        }
+    },
+    contact: {
+        title: "联系我",
+        list: [{
+                name: "博客",
+                context: "blog.52yimen.cn"
+            },
+            {
+                name: "邮箱",
+                context: "574686795@qq.com"
+            },
+            {
+                name: "QQ",
+                context: "574686795"
+            },
+            {
+                name: "微信",
+                context: "我不想告诉你"
+            }
+        ]
+    }
+}
